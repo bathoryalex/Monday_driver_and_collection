dd9dc5aa2e06d7d7396e5cd4f8493425b7536804
Bathory Alex <bathory.alex@gmail.com>
2016-04-12 12:51:58 +0200

    First task is done

---
+++ Monday Collections/MondayCollections/MondayCollections/StringOrganizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+
+namespace MondayCollections
+{
+    class StringOrganizer
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrayList = new ArrayList();
+
+            string userInput;
+
+            Console.WriteLine("Add strings! This program will sort them in alphabetical order. \nYou have to hit Enter after every word. When you are done hit Enter two times!");
+
+            while ((userInput = Console.ReadLine()) != "")
+            {
+                myArrayList.Add(userInput);
+            }
+
+
+            Console.WriteLine("The ArrayList initially contains the following values:");
+            PrintValues(myArrayList);
+
+            myArrayList.Sort();
+
+            Console.WriteLine("After sorting:");
+            PrintValues(myArrayList);
+
+            Console.ReadLine();
+        }
+
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+                Console.WriteLine("   {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+
