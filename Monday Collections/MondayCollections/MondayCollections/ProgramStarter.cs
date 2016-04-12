21666aa0a29e8d4d94550cef17a95de375b72741
Bathory Alex <bathory.alex@gmail.com>
2016-04-12 15:17:31 +0200

    Exercise 3 is done

--- Monday Collections/MondayCollections/MondayCollections/ProgramStarter.cs
+++ Monday Collections/MondayCollections/MondayCollections/ProgramStarter.cs
@@ -8,8 +8,10 @@ namespace MondayCollections
         {
             //Console.WriteLine("Exercise 1: String organizer:\n");
             //StringOrganizer.StringOrg();
-            Console.WriteLine("Exercise 2: Integers in reverse sequence:\n");
-            IntegersReversed.IntegersRev();
+            //Console.WriteLine("Exercise 2: Integers in reverse sequence:\n");
+            //IntegersReversed.IntegersRev();
+            Console.WriteLine("Exercise 3: Written numbers to digits:\n");
+            WrittenNumbersToDigits.wNumToDig();
         } 
     }
 }
--- Monday Collections/MondayCollections/MondayCollections/WrittenNumbersToDigits.cs
+++ Monday Collections/MondayCollections/MondayCollections/WrittenNumbersToDigits.cs
@@ -1,7 +1,40 @@
-﻿namespace MondayCollections
+﻿using System.Collections.Generic;
+using System;
+
+namespace MondayCollections
 {
     public class WrittenNumbersToDigits
     {
-         
+        public static void wNumToDig()
+        {
+            Dictionary <string, int> dictionary = new Dictionary<string, int>();
+
+	        dictionary.Add("zero", 0);
+            dictionary.Add("one", 1);
+	        dictionary.Add("two", 2);
+	        dictionary.Add("three", 3);
+	        dictionary.Add("four", 4);
+	        dictionary.Add("five", 5);
+	        dictionary.Add("six", 6);
+	        dictionary.Add("seven", 7);
+	        dictionary.Add("eight", 8);
+	        dictionary.Add("nine", 9);
+
+            string userInput = Console.ReadLine();
+
+            
+            foreach (KeyValuePair<string, int> var in dictionary)
+            {
+
+                if (userInput.Contains(var.Key))
+                {
+                    userInput = userInput.Replace(var.Key, var.Value.ToString());
+                }
+            }
+
+            Console.WriteLine(userInput);
+
+            Console.ReadLine();
+        }
     }
 }
